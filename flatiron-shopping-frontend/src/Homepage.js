4decd8540555c8c07606a2139a1dfae23fb30774
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-01-21 22:09:30 -0500

    filters functional, still needs work on quantity added to cart, display of cart, and login, signup, and persisting orders

--- flatiron-shopping-frontend/src/Homepage.js
+++ flatiron-shopping-frontend/src/Homepage.js
@@ -13,13 +13,13 @@ export default class Homepage extends React.Component{
 
 
     render() {
+        console.log(this.props.items)
         return (
-
 <div>
 <br></br>
         <h1 className="shopping-app-h1">Flatiron Shopping App</h1>
         <br></br>
-        <SearchBar />
+        <SearchBar settingBooleanForSorting={this.props.settingBooleanForSorting}/>
         <br></br>
         <div className= "main-item-container">
       <ItemContainer items={this.props.items} buttonToAddToCartClicked={this.props.buttonToAddToCartClicked} itemClickedOn={this.props.itemClickedOn} returnToItemList={this.props.returnToItemList} expandItem={this.props.expandItem} selectedToExpand={this.props.selectedToExpand}/>
